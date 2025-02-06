dba7f781ca4a59487b08536b8acf70929c4391c1
nayeem01 <nayeem.khan@enosisbd.com>
2025-02-06 12:13:32 +0600

    Added a base structure for next.js project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black">
+      <div>
+        <h1>
+          Hello, Portfolio
+        </h1>
+      </div>
+    </main>
+  );
+}
