35f1d6838c32a5e46b5550d5b08e41ec565dbba3
md-nayeem-khan <nayeem1505@gmail.com>
2025-02-09 07:45:25 +0600

    Added footer section in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,11 @@ import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -19,6 +20,7 @@ const Home = () => {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Footer />
       </div>
     </main>
   );
